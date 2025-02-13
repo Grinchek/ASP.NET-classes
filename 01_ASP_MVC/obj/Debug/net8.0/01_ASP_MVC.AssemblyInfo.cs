52918465d23bb48c64cb1a4f3aa3f1d0bdb87c86
Grinchek <warning686@gmail.com>
2025-02-13 18:22:48 +0200

    Implement CRUD for products

--- 01_ASP_MVC/obj/Debug/net8.0/01_ASP_MVC.AssemblyInfo.cs
+++ 01_ASP_MVC/obj/Debug/net8.0/01_ASP_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_ASP_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7c9d87723a932b5f7ab4186f67dd7f50143d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_ASP_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_ASP_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
